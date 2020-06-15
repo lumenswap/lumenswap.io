27a2c05bc19a28d183bad4e9c1ff6acd0120b18f
mersede <mersede.rahbar@gmail.com>
2020-06-15 17:17:10 +0430

    add confirm swap modal

---
+++ src/shared/components/ModalContent/ConfirmSwapContent/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import btcLogo from 'Root/assets/images/btc-logo.png';
+import ethLogo from 'Root/assets/images/eth-logo.png';
+import arrowDown from 'Root/assets/images/arrow-down.png';
+import InfoItem from 'Root/shared/components/InfoItem';
+import styles from './styles.less';
+
+const ConfirmSwapContent = (props) => {
+  const confirmInfo = [
+    {
+      subject: 'Minimum received', value: '2952 ETH', tooltipId: 'rc-eth', tooltipInfo: 'This is a tooltip',
+    },
+    {
+      subject: 'Price spread', value: '2%', tooltipId: 'price-spread', tooltipInfo: 'This is a tooltip',
+    },
+    {
+      subject: 'Set slippage tolerance', value: '0.1%', tooltipId: 'tolerance', tooltipInfo: 'This is a tooltip',
+    },
+  ];
+  return (
+    <>
+      <div className="row justify-content-between h-100 align-items-center" style={{ marginTop: '-12px' }}>
+        <div className={classNames('col-auto', styles.value)}>5</div>
+        <div className={classNames('col-auto', styles.crypto)}>
+          <img src={btcLogo} alt="logo" />BTC
+          <span className={styles.web}>(amir.com)</span>
+        </div>
+      </div>
+      <img
+        src={arrowDown}
+        height="24px"
+        width="24px"
+        alt="arrow"
+      />
+      <div className="row justify-content-between h-100 align-items-center">
+        <div className={classNames('col-auto', styles.value)}>11.5</div>
+        <div className={classNames('col-auto', styles.crypto)}>
+          <img src={ethLogo} alt="logo" />ETH
+          <span className={styles.web}>(apay.com)</span>
+        </div>
+      </div>
+      <div className={styles.box}>
+        {confirmInfo.map((item, index) => (
+          <div key={index}>
+            <InfoItem item={item} />
+          </div>
+        ))}
+      </div>
+      <button
+        type="button"
+        className="button-primary-lg "
+        style={{ marginTop: '32px', marginBottom: '10px' }}
+      >Confirm
+      </button>
+    </>
+  );
+};
+
+ConfirmSwapContent.propTypes = {
+
+};
+
+export default ConfirmSwapContent;
--- src/views/ModalPage/index.js
+++ src/views/ModalPage/index.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import CustomModal from '../../shared/components/CustomModal';
 import ConfirmSendContent from '../../shared/components/ModalContent/ConfirmSendContent';
 import TokenContent from '../../shared/components/ModalContent/TokenContent';
+import ConfirmSwapContent
+  from '../../shared/components/ModalContent/ConfirmSwapContent';
 
 const ModalPage = () => {
-  const [confirmModal, toggleConfirmModal] = useState(false);
+  const [confirmSendModal, toggleConfirmSendModal] = useState(false);
+  const [confirmSwapModal, toggleConfirmSwapModal] = useState(false);
   const [tokenModal, toggleTokenModal] = useState(false);
   return (
     <div className="h-100" style={{ background: '#09112c' }}>
@@ -12,16 +15,30 @@ const ModalPage = () => {
         {/* confirm send modal */}
         <button
           className="btn btn-primary mr-3"
-          onClick={() => { toggleConfirmModal(true); }}
+          onClick={() => { toggleConfirmSendModal(true); }}
         >confirm send
         </button>
         <CustomModal
-          toggle={toggleConfirmModal}
-          modal={confirmModal}
+          toggle={toggleConfirmSendModal}
+          modal={confirmSendModal}
           title="Confirm Send"
           modalSize="360"
         >
           <ConfirmSendContent />
+        </CustomModal>
+        {/* confirm swap */}
+        <button
+          className="btn btn-primary mr-3"
+          onClick={() => { toggleConfirmSwapModal(true); }}
+        >Confirm Swap
+        </button>
+        <CustomModal
+          toggle={toggleConfirmSwapModal}
+          modal={confirmSwapModal}
+          title="Confirm Swap"
+          modalSize="360"
+        >
+          <ConfirmSwapContent />
         </CustomModal>
         {/* token modal */}
         <button
