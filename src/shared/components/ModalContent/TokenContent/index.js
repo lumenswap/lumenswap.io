a9ede4ae460de184503a2f95b8f13c4de4768faa
mersede <mersede.rahbar@gmail.com>
2020-06-15 16:59:02 +0430

    add token modal

--- src/shared/components/ModalContent/TokenContent/index.js
+++ src/shared/components/ModalContent/TokenContent/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import btcLogo from 'Root/assets/images/btc-logo.png';
+import ethLogo from 'Root/assets/images/eth-logo.png';
+import xlmLogo from 'Root/assets/images/xlm-logo.png';
 import styles from './styles.less';
 
-const TokenContent = (props) => (
-  <>
+const tokens = [
+  { name: 'BTC', logo: btcLogo, web: 'amir.com' },
+  { name: 'ETH', logo: ethLogo, web: 'apay.com' },
+  { name: 'BTC', logo: btcLogo, web: 'amir.com' },
+  { name: 'ETH', logo: ethLogo, web: 'apay.com' },
+  { name: 'BTC', logo: btcLogo, web: 'amir.com' },
+  { name: 'ETH', logo: ethLogo, web: 'apay.com' },
+  { name: 'XLM', logo: xlmLogo, web: 'web.com' },
+];
 
-  </>
-);
+const TokenContent = (props) => {
+  const [searchString, setSearchString] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = (e) => {
+    setSearchString(e.target.value);
+  };
+
+  useEffect(() => {
+    const results = tokens.filter((token) => token.name.toLowerCase().includes(searchString.toLowerCase()));
+    setSearchResults(results);
+  }, [searchString]);
+
+  const item = (data) => (
+    <div className="row justify-content-between mb-3 h-100 align-items-center">
+      <div className={classNames(styles.crypto, 'col-auto')}>
+        <img
+          src={data.logo}
+          alt="logo"
+        />
+        {data.name}
+      </div>
+      <div className={classNames('col-auto', styles.web)}>{data.web}</div>
+    </div>
+  );
+
+  return (
+    <div className="row">
+      <div className="col-12">
+        <input
+          type="text"
+          className={classNames(styles.input, 'form-control primary-input')}
+          value={searchString}
+          onChange={(e) => { handleChange(e); }}
+          placeholder="Search name or paste address"
+        />
+        {searchResults.map((token, index) => (
+          <div key={index} className="mt-3 pt-2">
+            {item(token)}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 TokenContent.propTypes = {
 
--- src/views/ModalPage/index.js
+++ src/views/ModalPage/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import CustomModal from '../../shared/components/CustomModal';
 import ConfirmSendContent from '../../shared/components/ModalContent/ConfirmSendContent';
+import TokenContent from '../../shared/components/ModalContent/TokenContent';
 
 const ModalPage = () => {
   const [confirmModal, toggleConfirmModal] = useState(false);
+  const [tokenModal, toggleTokenModal] = useState(false);
   return (
     <div className="h-100" style={{ background: '#09112c' }}>
       <div className="container mt-5">
+        {/* confirm send modal */}
         <button
-          className="btn btn-primary"
+          className="btn btn-primary mr-3"
           onClick={() => { toggleConfirmModal(true); }}
         >confirm send
         </button>
@@ -19,6 +22,20 @@ const ModalPage = () => {
           modalSize="360"
         >
           <ConfirmSendContent />
+        </CustomModal>
+        {/* token modal */}
+        <button
+          className="btn btn-primary"
+          onClick={() => { toggleTokenModal(true); }}
+        >select token
+        </button>
+        <CustomModal
+          toggle={toggleTokenModal}
+          modal={tokenModal}
+          title="Select token"
+          modalSize="360"
+        >
+          <TokenContent />
         </CustomModal>
       </div>
     </div>
