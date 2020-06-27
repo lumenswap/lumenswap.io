657f3f67e5d84c23a593887c4781c1d24747f4e3
Ali Movahedi <ali_movahedi@aol.com>
2020-06-27 21:24:03 +0430

    rename tokens to assets in tooltip

--- src/constants/info.js
+++ src/constants/info.js
@@ -1,3 +1,3 @@
-export const path = 'This represents the best path to convert the specified tokens based on the order books on the network. ';
+export const path = 'This represents the best path to convert the specified assets based on the order books on the network. ';
 export const minimumReceived = 'This is the minimum amount of an asset you’ll receive after the swap';
 export const slippageTolerance = 'This shows the minimum you’ll receive. Higher percentages mean you’ll receive less.';
