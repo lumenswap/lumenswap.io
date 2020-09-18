873a438f932598bc3500f47389e8d9d2715f2155
Ali Movahedi <ali_movahedi@aol.com>
2020-09-18 21:05:07 +0430

    connect sidebar in mobile view to wallet

--- src/shared/components/Layout/BaseLayout/ResponsiveMenu/index.js
+++ src/shared/components/Layout/BaseLayout/ResponsiveMenu/index.js
@@ -1,68 +1,112 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { slide as Menu } from 'react-burger-menu';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import leftArrow from 'src/assets/images/long-arrow-left.svg';
-import {orderPages} from 'src/constants/routes';
+import { orderPages } from 'src/constants/routes';
 import logo from 'src/assets/images/xlm-logo.png';
 import styles from './styles.module.scss';
+import questionLogo from 'src/assets/images/question.png';
+import { useSelector } from 'react-redux';
+import defaultTokens from 'src/tokens/defaultTokens';
+import XLM from 'src/tokens/XLM';
+import minimizeAddress from 'src/helpers/minimizeAddress';
 
-const ResponsiveMenu = props => {
+const ResponsiveMenu = (props) => {
   const [isOpen, toggleOpen] = useState(false);
   const handleStateChange = (state) => {
-    toggleOpen(state.isOpen)
-  }
+    toggleOpen(state.isOpen);
+  };
+
+  const userToken = useSelector((state) => state.userToken)
+    .sort((a, b) => {
+      if (a.asset_type === 'native') {
+        return -1;
+      }
+      if (b.asset_type === 'native') {
+        return 1;
+      }
+
+      return 0;
+    })
+    .map((token) => {
+      if (token.asset_type === 'native') {
+        return {
+          value: 'XLM',
+          logo: XLM.logo,
+          balance: token.balance,
+        };
+      }
+      const found = defaultTokens.find(
+        (oneToken) =>
+          oneToken.code === token.asset_code &&
+          oneToken.issuer === token.asset_issuer
+      );
+      if (!found) {
+        return {
+          value: token.asset_code,
+          logo: questionLogo,
+          balance: token.balance,
+        };
+      }
+
+      return {
+        value: found.code,
+        logo: found.logo,
+        balance: token.balance,
+      };
+    });
 
   return (
-      <Menu
-          pageWrapId="page-wrap"
-          outerContainerId="header"
-          width={314}
-          right
-          burgerButtonClassName={ styles.nav }
-          isOpen={isOpen}
-          onStateChange={state => handleStateChange(state)}
-          customBurgerIcon={
-            <button type="button" className="btn p-0">
-              <img src={leftArrow} width="30px" height="24px" alt="icon"/>
-            </button>
-          }
-      >
-        <div className={styles.box}>
-          <button
-              className={classNames('icon-multiplied btn p-0', styles.close)}
-              onClick={() => toggleOpen(false)}
-          />
-          <div className={classNames('vertical-scroll', styles.scroll)}>
-            {[0,1,2,3,4,5,7,8 ].map(item => (
-                <div className={styles.crypto} key={item}>
-                  <div className="row align-items-center h-100">
-                    <div className="col-auto">
-                      <img src={logo} className={styles.logo} alt="logo"/>
-                    </div>
-                    <div className={classNames('col-auto pl-0', styles['item-box'])}>
-                      <h5 className={styles.name}>XLM</h5>
-                      <p className={styles.more}>stellarterm.com</p>
-                    </div>
-                    <div className="col-auto ml-auto">
-                      <p className={styles.count}>10</p>
-                    </div>
-                  </div>
+    <Menu
+      pageWrapId="page-wrap"
+      outerContainerId="header"
+      width={314}
+      right
+      burgerButtonClassName={styles.nav}
+      isOpen={isOpen}
+      onStateChange={(state) => handleStateChange(state)}
+      customBurgerIcon={
+        <button type="button" className="btn p-0">
+          <img src={leftArrow} width="30px" height="24px" alt="icon" />
+        </button>
+      }
+    >
+      <div className={styles.box}>
+        <button
+          className={classNames('icon-multiplied btn p-0', styles.close)}
+          onClick={() => toggleOpen(false)}
+        />
+        <div className={classNames('vertical-scroll', styles.scroll)}>
+          {userToken.map((item) => (
+            <div className={styles.crypto} key={item}>
+              <div className="row align-items-center h-100">
+                <div className="col-auto">
+                  <img src={item.logo} className={styles.logo} alt="logo" />
                 </div>
-            ))}
-          </div>
+                <div
+                  className={classNames('col-auto pl-0', styles['item-box'])}
+                >
+                  <h5 className={styles.name}>{item.value}</h5>
+                  <p className={styles.more}>{item.balance}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className={styles.options}>
-          <Link className={styles.link} to={orderPages}>Orders</Link>
-          <Link className={styles.link} to="/">Logout</Link>
-        </div>
-      </Menu>
+      </div>
+      <div className={styles.options}>
+        <Link className={styles.link} to={orderPages}>
+          Orders
+        </Link>
+        <Link className={styles.link} to="/">
+          Logout
+        </Link>
+      </div>
+    </Menu>
   );
 };
 
-ResponsiveMenu.propTypes = {
-
-};
+ResponsiveMenu.propTypes = {};
 
 export default ResponsiveMenu;
