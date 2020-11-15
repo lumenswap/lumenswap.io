58d7ddbedb4d8c3d4a393e31611736a1d53e7e05
Ali Movahedi <ali_movahedi@aol.com>
2020-11-15 14:22:06 +0330

    update site for AFR token

--- src/tokens/AFR.js
+++ src/tokens/AFR.js
@@ -3,6 +3,6 @@ import logo from 'src/assets/images/afreum.png';
 export default {
   code: 'AFR',
   logo,
-  web: 'afreum.como',
+  web: 'afreum.com',
   issuer: 'GBX6YI45VU7WNAAKA3RBFDR3I3UKNFHTJPQ5F6KOOKSGYIAM4TRQN54W',
 };
