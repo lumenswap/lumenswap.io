a6649ee97df4ccd6a57910efa8d74c8025df0acf
Ali Movahedi <ali_movahedi@aol.com>
2020-11-15 13:49:46 +0330

    add two token

---
+++ src/tokens/AFR.js
@@ -0,0 +1,8 @@
+import logo from 'src/assets/images/question.png';
+
+export default {
+  code: 'AFR',
+  logo,
+  web: 'afreum.como',
+  issuer: 'GBX6YI45VU7WNAAKA3RBFDR3I3UKNFHTJPQ5F6KOOKSGYIAM4TRQN54W',
+};
---
+++ src/tokens/DEB.js
@@ -0,0 +1,8 @@
+import logo from 'src/assets/images/question.png';
+
+export default {
+  code: 'DEB',
+  logo,
+  web: 'www.drivedeb.com',
+  issuer: 'GB6G343Z2IHS3W2V6QGGEM622V5S4UUS7C2NEOZFPYG3DCEB256FAOZA',
+};
