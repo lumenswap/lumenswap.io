5ab7f80df6cbe8a491d7561a4cf85bc3e41f38e3
Alberto De Checchi <agenio@gmail.com>
2021-02-19 23:17:27 +0000

    Add KTHOUSE token

---
+++ src/tokens/KTHOUSE.js
@@ -0,0 +1,9 @@
+import logo from 'src/assets/images/kthouse.png';
+
+export default {
+  code: 'KTHOUSE',
+  logo,
+  web: 'kthouse.co',
+  issuer: 'GB5BK7HJK7SJGJXN4X6BV2QBEWGXVMEZJRGKWBFU6PK6BV4DW7WOQ27W',
+};
+
