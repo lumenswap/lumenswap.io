1a5c08f43954782dfdd59247cfd66bb3d496c2ea
mersede <mersede.rahbar@gmail.com>
2021-03-28 16:25:55 +0430

    add connect wallet btn

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import { ReactComponent as Logo } from 'assets/images/logo.svg';
 import { Link } from 'react-router-dom';
 import CustomDropdown from 'components/Dropdown';
+import Button from 'components/Button';
 import styles from './styles.module.scss';
 
 const Header = () => (
@@ -10,7 +11,10 @@ const Header = () => (
       <li><Link to="/">Swap</Link></li>
       <li><Link to="/">Stats <span className="icon-external" /></Link></li>
     </ul>
-    <CustomDropdown height="40px" width="160px" />
+    {/* connected */}
+    {/* <CustomDropdown height="40px" width="160px" /> */}
+    {/* not connected */}
+    <Button variant="secondary" content="Connect Wallet" className={styles.btn} />
   </div>
 );
 
