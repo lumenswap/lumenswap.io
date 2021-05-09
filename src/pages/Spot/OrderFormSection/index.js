a0a0d888dd6654cd5441ec0ce4661f61a33effc0
mersede <mersede.rahbar@gmail.com>
2021-05-09 17:02:08 +0430

    add order form

--- src/pages/Spot/OrderFormSection/index.js
+++ src/pages/Spot/OrderFormSection/index.js
@@ -1,25 +1,45 @@
 import InputGroup from 'components/InputGroup';
 import CustomSlider from 'components/CustomSlider';
+import Button from 'components/Button';
+import capitalizeFirstLetter from 'helpers/capitalizeFirstLetter';
+import styles from '../styles.module.scss';
+
+const generateForm = (type, value) => (
+  <form>
+    <div className="d-flex justify-content-between align-items-center mb-2">
+      <div className={styles['form-title']}>{capitalizeFirstLetter(type)} XLM</div>
+      <div className={styles['form-value']}>{value}</div>
+    </div>
+    <div className="mb-2">
+      <InputGroup type="number" name="price" rightLabel="USDC" leftLabel="Price" />
+    </div>
+    <div className="mb-3">
+      <InputGroup type="number" name="amount" rightLabel="XLM" leftLabel="Amount" />
+    </div>
+    <div className="mb-3">
+      <CustomSlider />
+    </div>
+    <div className="mb-2">
+      <InputGroup type="number" name="amount" rightLabel="USDC" leftLabel="Total" />
+    </div>
+    <Button
+      htmlType="submit"
+      variant={type}
+      content={`${capitalizeFirstLetter(type)} XLM`}
+      fontWeight={500}
+      className={styles.button}
+    />
+  </form>
+);
 
 const OrderFormSection = () => (
-  <div className="row">
-    <div className="col-6">
-      <form>
-        <div className="mb-2">
-          <InputGroup type="number" name="price" rightLabel="USDC" leftLabel="Price" />
-        </div>
-        <div>
-          <InputGroup type="number" name="amount" rightLabel="XLM" leftLabel="Amount" />
-        </div>
-        <div className="my-3">
-          <CustomSlider />
-        </div>
-        <div style={{ marginBottom: '12px' }}>
-          <InputGroup type="number" name="amount" rightLabel="USDC" leftLabel="Total" />
-        </div>
-      </form>
+  <div className="row" style={{ margin: '0 -24px' }}>
+    <div className="col-6 px-4">
+      {generateForm('buy', '0.383 USDC')}
     </div>
-    <div className="col-6" />
+    <div className="col-6 px-4">
+      {generateForm('sell', '0.383 XLM')}
+    </div>
   </div>
 );
 
