09a939913370bb372f8960dd8b0cc9186df6b526
Ali Movahedi <ali_movahedi@aol.com>
2021-05-27 17:14:42 +0430

    fix order form section

--- src/pages/Spot/OrderSection/index.js
+++ src/pages/Spot/OrderSection/index.js
@@ -5,6 +5,13 @@ import BN from 'helpers/BN';
 import sevenDigit from 'helpers/sevenDigit';
 import { useEffect, useRef, useState } from 'react';
 
+function setCustomOrderPriceInput(data) {
+  setCustomOrderPriceAction({
+    buy: data.asks[0]?.price || '',
+    sell: data.bids[0]?.price || '',
+  });
+}
+
 const OrderSection = ({ appSpotPair }) => {
   const [orderBookData, setOrderBookData] = useState(null);
   const intervalRef = useRef(null);
@@ -22,24 +29,16 @@ const OrderSection = ({ appSpotPair }) => {
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
       setOrderBookData(null);
-      fetchingOrderAPICallWrapper().then((data) => {
-        setCustomOrderPriceAction({
-          buy: data.asks[0].price,
-          sell: data.bids[0].price,
-        });
-      });
+      fetchingOrderAPICallWrapper()
+        .then(setCustomOrderPriceInput);
       intervalRef.current = setInterval(fetchingOrderAPICallWrapper, 10000);
     }
   }, [appSpotPair.base, appSpotPair.counter]);
 
   useEffect(() => {
     if (!intervalRef.current) {
-      fetchingOrderAPICallWrapper().then((data) => {
-        setCustomOrderPriceAction({
-          buy: data.asks[0].price,
-          sell: data.bids[0].price,
-        });
-      });
+      fetchingOrderAPICallWrapper()
+        .then(setCustomOrderPriceInput);
       intervalRef.current = setInterval(fetchingOrderAPICallWrapper, 10000);
     }
 
@@ -50,7 +49,7 @@ const OrderSection = ({ appSpotPair }) => {
 
   let total = 0;
 
-  if (orderBookData?.asks[0]) {
+  if (orderBookData?.asks[0] && orderBookData?.bids[0]) {
     total = sevenDigit((new BN(orderBookData.asks[0].price).plus(orderBookData.bids[0].price))
       .div(2)
       .toFixed(7));
