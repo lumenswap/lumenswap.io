ac808d1ac3d7717181a69db3ec546cbd4b7e4d19
mersede <mersede.rahbar@gmail.com>
2021-06-03 13:36:55 +0430

    edit text

--- src/blocks/SelectAsset/index.js
+++ src/blocks/SelectAsset/index.js
@@ -82,7 +82,7 @@ const SelectAsset = ({
     <div className={styles.container}>
       <Input
         type="text"
-        placeholder="Search asset pair"
+        placeholder="Search asset code"
         name="search"
         fontSize={14}
         onChange={(e) => {
