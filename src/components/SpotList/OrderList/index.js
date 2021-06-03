5737608f4850cb71b712040ba7a30aebee37922b
mersede <mersede.rahbar@gmail.com>
2021-06-03 13:27:07 +0430

    fix gradiant hover

--- src/components/SpotList/OrderList/index.js
+++ src/components/SpotList/OrderList/index.js
@@ -1,11 +1,16 @@
-import { setCustomOrderPriceAction } from 'actions/customOrderPrice';
 import classNames from 'classnames';
+import { useState } from 'react';
 import BN from 'helpers/BN';
 import sevenDigit from 'helpers/sevenDigit';
+import { setCustomOrderPriceAction } from 'actions/customOrderPrice';
 import styles from '../styles.module.scss';
 
 function generateProgressStyle(percent, isSell) {
   return `linear-gradient(to left, ${isSell ? '#f5dce6' : '#e8eedc'} ${percent}%, transparent 0%)`;
+}
+
+function generateHoverStyle(percent, isSell) {
+  return `linear-gradient(to left, ${isSell ? '#f5dce6' : '#e8eedc'} ${percent}%, #f5f5f7 0%)`;
 }
 
 function calculateAmountAndTotal(isSell, i) {
@@ -41,6 +46,35 @@ const OrderList = ({
     forUsingData = forUsingData.sort((a, b) => -1 * new BN(a.price).comparedTo(b.price));
   }
 
+  const orderItem = (row) => {
+    const [hover, setHover] = useState(false);
+    return (
+      <div
+        className={classNames(styles.progress, isSell ? styles.sell : styles.buy)}
+        style={{
+          background: hover
+            ? generateHoverStyle(row.percent, isSell) : generateProgressStyle(row.percent, isSell),
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div
+          className={styles['table-row']}
+          onClick={() => {
+            setCustomOrderPriceAction({
+              sell: row.price,
+              buy: row.price,
+            });
+          }}
+        >
+          <div className={styles['row-item']}>{sevenDigit(row.price)}</div>
+          <div className={styles['row-item']}>{sevenDigit(new BN(row.amount).div(row.price).toFixed(7))}</div>
+          <div className={styles['row-item']}>{sevenDigit(row.total.toFixed(7))}</div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles['table-container']}>
       <div className={classNames(styles.heading, styles['table-head'])} style={{ cursor: 'default' }}>
@@ -55,28 +89,8 @@ const OrderList = ({
       </div>
       <div className={styles['table-body']}>
         {forUsingData.map((row, rowIndex) => (
-          <div
-            key={`row-${rowIndex}`}
-          >
-            <div
-              className={classNames(styles.progress,
-                isSell ? styles.sell : styles.buy)}
-              style={{ background: generateProgressStyle(row.percent, isSell) }}
-            >
-              <div
-                className={styles['table-row']}
-                onClick={() => {
-                  setCustomOrderPriceAction({
-                    sell: row.price,
-                    buy: row.price,
-                  });
-                }}
-              >
-                <div className={styles['row-item']}>{sevenDigit(row.price)}</div>
-                <div className={styles['row-item']}>{sevenDigit(row.innerAmount)}</div>
-                <div className={styles['row-item']}>{sevenDigit(row.total.toFixed(7))}</div>
-              </div>
-            </div>
+          <div key={`row-${rowIndex}`}>
+            {orderItem(row)}
           </div>
         ))}
       </div>
