91f99d08900122a15739d01f43507f0441985eaf
TheLoom <ali_movahedi@aol.com>
2021-06-11 05:15:10 +0000

    fix USDS web

--- src/tokens/USDS.js
+++ src/tokens/USDS.js
@@ -3,6 +3,6 @@ import logo from './images/usds.png';
 export default {
   code: 'USDS',
   logo,
-  web: 'centre.io',
+  web: 'stably.io',
   issuer: 'GBK4AYTOYIAYT4UJECSQGMACUWLKYOOM4VCAUUZ3Y3FG5XD2LYK3FGK2',
 };
