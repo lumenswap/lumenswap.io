be6b298b404677dda439f840bd92cd967f67406f
TheLoom <ali_movahedi@aol.com>
2021-06-25 06:28:07 +0000

    remove www from doget address

--- src/tokens/DOGET.js
+++ src/tokens/DOGET.js
@@ -3,6 +3,6 @@ import logo from './images/doget.png';
 export default {
   code: 'DOGET',
   logo,
-  web: 'www.doge-token.com',
+  web: 'doge-token.com',
   issuer: 'GDOEVDDBU6OBWKL7VHDAOKD77UP4DKHQYKOKJJT5PR3WRDBTX35HUEUX',
 };
