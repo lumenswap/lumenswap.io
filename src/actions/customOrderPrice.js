56c5e7362b6fa6787ff9d442814b0919c8a2367b
shayan balaee <shayanfall79@gmail.com>
2021-07-29 15:19:26 +0430

    changed the usage of store

--- src/actions/customOrderPrice.js
+++ src/actions/customOrderPrice.js
@@ -1,8 +1,11 @@
-import store from 'store';
+import { initializeStore } from 'store';
 import actionTypes from './index';
 
 // eslint-disable-next-line
 export function setCustomOrderPriceAction(orderPrice) {
+  // todo: should ask about this
+  const store = initializeStore();
+
   store.dispatch({
     type: actionTypes.customOrderPrice.SET,
     toSet: orderPrice,
