591acd1ebe61758a1cc386f01be38b0cc59bbeed
shayan balaee <shayanfall79@gmail.com>
2021-07-29 15:26:11 +0430

    image importing changes

--- src/components/FetchDataLoading/index.js
+++ src/components/FetchDataLoading/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ReactComponent as Bars } from 'assets/images/bars.svg';
+import Bars from 'assets/images/bars.svg';
 import styles from './styles.module.scss';
 
 const FetchDataLoading = () => (
   <div className={styles.container}>
-    <Bars />
+    <img src={Bars.src} />
   </div>
 );
 
