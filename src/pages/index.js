808e327a07b8410ea27a95ea0bc5dd9a9108a62b
Ali Movahedi <ali_movahedi@aol.com>
2021-07-30 09:51:08 +0430

    remove index page

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Router from 'next/router';
-import { initializeStore } from 'store';
-
-const store = initializeStore();
-console.log(store);
-
-export default function Home() {
-  return null;
-}
