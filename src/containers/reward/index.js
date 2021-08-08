36706c8c1a2c87ad46b0885f9ca83a9653d2b4f4
Amirrk79 <amirhosseinkazemiseresht@aol.com>
2021-08-08 21:16:42 +0430

    add: not logged mode in reward page

--- src/containers/reward/index.js
+++ src/containers/reward/index.js
@@ -1,9 +1,16 @@
 import Header from 'components/Header';
 import InfoOfLSP from 'components/InfoOfLSP';
+import Image from 'next/image';
 import CTable from 'components/CTable';
+import { useSelector, useDispatch } from 'react-redux';
+import Button from 'components/Button';
+import { openConnectModal } from 'actions/modal';
 import styles from './styles.module.scss';
+import connectIcon from './SVG-icons/connectIcon.svg';
 
 const Reward = () => {
+  const dispatch = useDispatch();
+  const isLogged = useSelector((state) => state.user.logged);
   const walletBalance = 1000;
   const holderReward = 12;
   const tradeReward = 1000;
@@ -75,15 +82,42 @@ const Reward = () => {
 
       <div className={styles.main}>
         <div className={styles['page-title']}>Dashboard</div>
-        <div className={styles['info-LSP']}>
-          <InfoOfLSP
-            walletBalance={walletBalance}
-            holderReward={holderReward}
-            tradeReward={tradeReward}
-          />
-        </div>
-        <div className={styles['table-title']}>Last activity</div>
-        <CTable columns={tableHeaders} dataSource={tableRows} />
+        {isLogged ? (
+          <div>
+            <div className={styles['info-LSP']}>
+              <InfoOfLSP
+                walletBalance={walletBalance}
+                holderReward={holderReward}
+                tradeReward={tradeReward}
+              />
+            </div>
+            <div className={styles['table-title']}>Last activity</div>
+            <CTable columns={tableHeaders} dataSource={tableRows} />
+          </div>
+        )
+          : (
+            <div className="row justify-content-center">
+              <div className="col-auto">
+                <div className={styles['connect-container']}>
+                  <div className={styles['div-icon']}>
+                    <Image width="40" height="34" src={connectIcon} alt="icon" />
+                  </div>
+                  <div className={styles['connect-text']}>
+                    To see the reward statistics, please connect your account.
+                  </div>
+                  <Button
+                    variant="secondary"
+                    content="Connect Wallet"
+                    fontWeight="500"
+                    className={styles['connect-btn']}
+                    onClick={() => {
+                      dispatch(openConnectModal());
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
 
       </div>
 
