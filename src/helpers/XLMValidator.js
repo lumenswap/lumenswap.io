fe3334c3dc225a5977016ce0b3e283a119b5f4e5
Ali Movahedi <ali_movahedi@aol.com>
2021-08-09 17:17:20 +0430

    fix max XLM

--- src/helpers/XLMValidator.js
+++ src/helpers/XLMValidator.js
@@ -3,5 +3,5 @@ import BN from './BN';
 export function calculateMaxXLM(xlmBalance, subentry) {
   return new BN(xlmBalance)
     .minus((2 + subentry) * 0.5)
-    .minus(0.00001).toString();
+    .minus(0.005).toString();
 }
