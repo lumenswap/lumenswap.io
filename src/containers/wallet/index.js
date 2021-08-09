9fa1d5bd6bd29e315227e28f50a9ccf713ee188c
mersede <mersede.rahbar@gmail.com>
2021-08-09 13:26:27 +0430

    add asset table

--- src/containers/wallet/index.js
+++ src/containers/wallet/index.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import Header from 'components/Header';
 import classNames from 'classnames';
 import DetailedInfo from 'components/DetailedInfo';
+import Table from 'components/Table';
+import sampleLogo from 'assets/images/btc-logo.png';
 import styles from './styles.module.scss';
 
 const wallet = () => {
@@ -10,6 +12,31 @@ const wallet = () => {
     { title: 'Total balance', xlm: '55,45', value: '45,5' },
     { title: 'Reserved balance', xlm: '12,4', value: '13,4' },
   ];
+
+  const tableHeader = ['Assets', 'Balance', 'Action'];
+
+  const tableRows = () => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((row, index) => (
+    <tr key={index}>
+      <td>
+        <div className="d-flex align-items-center">
+          <img src={sampleLogo} width={32} height={32} alt="logo" />
+          <span className={styles['td-name']}>
+            <span>DAI</span>
+            <span>Dinora</span>
+          </span>
+        </div>
+      </td>
+      <td>70.733</td>
+      <td>
+        <div className="d-flex">
+          <a href="/" className="mr-5">Swap</a>
+          <a href="/" className="mr-5">Trade</a>
+          <a href="/">Send</a>
+        </div>
+      </td>
+    </tr>
+  ));
+
   return (
     <div>
       <Head>
@@ -21,6 +48,9 @@ const wallet = () => {
           <h1 className="title my-0">Wallet</h1>
           <div className={classNames(styles.card, styles['card-detail'])}>
             <DetailedInfo details={details} />
+          </div>
+          <div className={classNames(styles.card, styles['card-table'])}>
+            <Table className={styles.table} tableRows={tableRows()} tableHead={tableHeader} />
           </div>
         </div>
       </div>
