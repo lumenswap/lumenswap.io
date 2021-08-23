505818e6c7f684674469a00cc554ae4be0135e59
shayan balaee <shayanfall79@gmail.com>
2021-08-23 20:34:42 +0430

    new envs for lumenswap api

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = withImages(withTM({
     REACT_APP_METRIC_SERVER: process.env.REACT_APP_METRIC_SERVER,
     REACT_APP_LUMENSCAN_URL: process.env.REACT_APP_LUMENSCAN_URL,
     REACT_APP_HOST: process.env.REACT_APP_HOST,
+    REACT_APP_LUMEN_API: process.env.REACT_APP_LUMEN_API,
   },
   async redirects() {
     return [
