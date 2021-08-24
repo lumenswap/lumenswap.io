a59e61b3e2994675bd84bfd6f299109f872b3a33
shayan balaee <shayanfall79@gmail.com>
2021-08-24 18:45:13 +0430

    Implemented sorting for CTable

--- src/components/CTable/index.js
+++ src/components/CTable/index.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import styles from './styles.module.scss';
 
 const CTable = ({
-  columns, dataSource, className, noDataMessage: NoDataMessage,
+  columns,
+  dataSource,
+  className,
+  noDataMessage: NoDataMessage,
 }) => {
   if (!dataSource || dataSource === null) {
     return <NoDataMessage />;
-  } if (dataSource.length === 0) {
+  }
+  if (dataSource.length === 0) {
     return <NoDataMessage />;
   }
+
+  const [sortIndex, setSortIndex] = useState(null);
+  const [sortOrder, setSortOrder] = useState('desc');
+
+  const handleSort = (sortFunc) => {
+    dataSource.sort((a, b) => sortFunc(a, b, sortOrder));
+    setSortOrder(sortOrder === 'desc' ? 'asc' : 'desc');
+  };
+
   return (
     <div className={className ?? styles['table-container']}>
       <table className={styles.table}>
-
         <tr className={styles['header-table']}>
           {columns.map((title) => (
             <th
               style={title.style ?? { width: `${100 / columns.length}%` }}
               key={title.key}
             >
-              <span>{title.title}</span>
+              <span style={{ position: 'relative' }}>{title.title} {title.sortFunc && (
+                <span className={styles.sort} onClick={() => handleSort(title.sortFunc)} />
+              )}
+              </span>
+
             </th>
           ))}
         </tr>
@@ -28,19 +44,14 @@ const CTable = ({
         {dataSource.map((data) => (
           <tr className={styles.row} key={data.key ?? nanoid(6)}>
             {columns.map((column) => (
-              <td
-                className={styles['row-item']}
-              >
+              <td className={styles['row-item']}>
                 <section>
-                  {column.render
-                    ? column.render(data)
-                    : data[column.dataIndex]}
+                  {column.render ? column.render(data) : data[column.dataIndex]}
                 </section>
               </td>
             ))}
           </tr>
         ))}
-
       </table>
     </div>
   );
