c9a54e830ff487d8ddccb4f8e80cabc51601f000
Ali Movahedi <ali_movahedi@aol.com>
2021-08-25 19:01:24 +0430

    remove dollar sign from market page

--- src/containers/market/KnownAssets.js
+++ src/containers/market/KnownAssets.js
@@ -124,7 +124,7 @@ function KnownAssets({ assets, searchQuery }) {
       dataIndex: 'volume24h',
       key: '6',
       sortFunc: (a, b, order) => (order === 'asc' ? a.volume24h - b.volume24h : b.volume24h - a.volume24h),
-      render: (data) => `$${numeral(data.volume24h).format('0.[0]a')} ${
+      render: (data) => `${numeral(data.volume24h).format('0.[0]a')} ${
         data.pair.counter.code
       }`,
     },
