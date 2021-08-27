ec75dc80e2b6ce04f6be5c8109d206140203c0c6
Ali Movahedi <ali_movahedi@aol.com>
2021-08-27 18:01:50 +0430

    change server port

--- server.js
+++ server.js
@@ -21,14 +21,16 @@ if (!dev) {
   });
 }
 
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
   createServer((req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
