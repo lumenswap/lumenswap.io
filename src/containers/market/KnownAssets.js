9a0f1c27af2a2583076803ee589d6c45c83787b6
Ali Movahedi <ali_movahedi@aol.com>
2021-08-28 13:00:02 +0430

    remove dollar sign

--- src/containers/market/KnownAssets.js
+++ src/containers/market/KnownAssets.js
@@ -123,7 +123,7 @@ function KnownAssets({ assets, searchQuery }) {
       dataIndex: 'volume24h',
       key: '6',
       sortFunc: (a, b, order) => (order === 'asc' ? a.volume24h - b.volume24h : b.volume24h - a.volume24h),
-      render: (data) => `$${numeral(data.volume24h).format('0.[0]a')} ${data.pair.base.code}`,
+      render: (data) => `${numeral(data.volume24h).format('0.[0]a')} ${data.pair.base.code}`,
     },
   ];
 
