90ad203e90451c6339073975c06659aafa315736
mersede <mersede.rahbar@gmail.com>
2021-09-01 14:50:03 +0430

    edit 404 page

--- src/containers/404/index.js
+++ src/containers/404/index.js
@@ -1,39 +1,35 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import Head from 'next/head';
+import classNames from 'classnames';
+
+import Lusi from 'assets/images/lusi';
+import Spaceship from 'assets/images/spaceship';
+
 import styles from './styles.module.scss';
-import NotFoundIcon from '../../assets/images/404.png';
 
 function NotFound() {
   return (
-    <div className="container-fluid">
+    <>
       <Head>
         <title>
           404 - Not Found
         </title>
       </Head>
-      <div className="row justify-content-center">
-        <div className={styles.main}>
-          <Image
-            src={NotFoundIcon}
-            width="633px"
-            height="352px"
-          />
-          <span className={styles.error}>Oh No! Error 404</span>
-          <span className={styles.info}>
-            The page you are looking for isn’t found!
-            we suggest you back to home
-          </span>
-          <Link href="/">
-            <a className={styles.link}>
-              <div className={styles.btn}>
-                <span>Back to home</span>
-              </div>
-            </a>
-          </Link>
+      <div className={styles.main}>
+        <div className="container-fluid">
+          <div className={styles.svg}>
+            <Spaceship />
+            <div className={classNames(styles.lusi, styles['up-down'])}>
+              <Lusi />
+            </div>
+          </div>
+          <div className={styles.title}>Oh No! Error 404</div>
+          <p className={styles.text}>
+            The page you are looking for isn’t found! we suggest you back to home
+          </p>
+          <a href="/" className={classNames(styles.link, styles.btn)}>Back to home</a>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
