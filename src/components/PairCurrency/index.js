0bd80209466b68293954a4839613f9388ab33969
mersede <mersede.rahbar@gmail.com>
2021-09-14 13:03:41 +0430

    add pool page

---
+++ src/components/PairCurrency/index.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+import styles from './styles.module.scss';
+
+const PairCurrency = ({ size, source }) => {
+  const margin = size / 3;
+  const imagesSize = (size * 2) - margin;
+  return (
+    <div className={styles.images} style={{ width: `${imagesSize}px` }}>
+      <div className={styles.container}>
+        <Image
+          src={source[0]}
+          width={size}
+          height={size}
+        />
+      </div>
+      <div className={styles.container} style={{ marginLeft: `-${margin}px` }}>
+        <Image
+          src={source[1]}
+          width={size}
+          height={size}
+          alt="currency"
+        />
+      </div>
+    </div>
+  );
+};
+
+PairCurrency.propTypes = {
+  size: PropTypes.number.isRequired,
+  source: PropTypes.array.isRequired,
+};
+
+export default PairCurrency;
---
+++ src/containers/pool/index.js
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import classNames from 'classnames';
+
+import ObmHeader from 'components/ObmHeader';
+import Button from 'components/Button';
+import Table from 'components/Table';
+import PairCurrency from 'components/PairCurrency';
+import btcLogo from 'assets/images/btc-logo.png';
+import usdLogo from 'assets/images/usd-coin-usdc.png';
+
+import styles from './styles.module.scss';
+
+const tableHeader = ['Pool', 'Amount'];
+
+const tableRows = () => [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row) => (
+  <tr key={row}>
+    <td width="50%">
+      <div className="d-flex">
+        <PairCurrency size={22} source={[btcLogo, usdLogo]} />
+        <span className={styles.pair}>XLM/USDC</span>
+      </div>
+    </td>
+    <td>1000 USDC/ 200 XLM</td>
+  </tr>
+));
+
+const PoolPage = () => (
+  <div className="container-fluid">
+    <Head>
+      <title>Pool | Lumenswap</title>
+    </Head>
+    <ObmHeader />
+    <div className={classNames('layout main', styles.main)}>
+      <div className="row justify-content-center">
+        <div className="col-xl-8 col-lg-10 col-md-11 col-sm-12 col-12 px-xl-5 px-lg-3 px-md-3 px-sm-3 px-3">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className={styles.label}>Pools</div>
+            <Button
+              variant="primary"
+              content="Add Liquidity"
+              className={styles.btn}
+            />
+          </div>
+          <Table
+            tableRows={tableRows()}
+            tableHead={tableHeader}
+            className={styles.table}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default PoolPage;
