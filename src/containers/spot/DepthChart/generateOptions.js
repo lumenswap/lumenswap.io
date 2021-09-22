b0b76562cf06755c6d5824424836ffbdcc05acfc
shayan balaee <shayanfall79@gmail.com>
2021-09-22 11:22:41 +0330

    added depth chart

---
+++ src/containers/spot/DepthChart/generateOptions.js
@@ -0,0 +1,50 @@
+export default (data) => ({
+  color: ['#74a700', '#ea0070'],
+
+  tooltip: {
+    trigger: 'item',
+    axisPointer: {
+      type: 'cross',
+    },
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: data?.xAxisSteps,
+    },
+  ],
+  yAxis: [
+    {
+      type: 'value',
+    },
+  ],
+  series: [
+    {
+      name: 'Bids',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: data?.bids.reverse().concat(Array.from({ length: data?.asks.length }).fill(null)),
+    },
+    {
+      name: 'Asks',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: Array.from({ length: data?.bids.length }).fill(null).concat(data?.asks),
+    },
+  ],
+});
---
+++ src/containers/spot/DepthChart/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import ReactECharts from 'echarts-for-react';
+import Loading from 'components/Loading';
+import { fetchOrderBookAPI } from 'api/stellar';
+import aggregateData from './aggregateData';
+import generateOptions from './generateOptions';
+import styles from './styles.module.scss';
+
+const DepthChart = ({ appSpotPair }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+    fetchOrderBookAPI(appSpotPair.base, appSpotPair.counter, { limit: 25 }).then((res) => {
+      setData(aggregateData(res));
+    });
+  }, [appSpotPair]);
+
+  const option = generateOptions(data);
+
+  if (data === null) {
+    return (
+      <div className="d-flex align-items-center justify-content-center p-5">
+        <Loading size={50} />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.chartContainer}>
+      <ReactECharts className={styles.chart} option={option} lazyUpdate />
+    </div>
+  );
+};
+
+export default DepthChart;
