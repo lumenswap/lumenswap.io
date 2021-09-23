dc91b17e1fa69db6f5d8e5247cdb446d279b05f0
shayan balaee <shayanfall79@gmail.com>
2021-09-23 16:40:12 +0330

    fixed price not changing for pairs with no data

--- src/containers/spot/OrderSection/index.js
+++ src/containers/spot/OrderSection/index.js
@@ -29,11 +29,16 @@ const OrderSection = ({ appSpotPair, price, setPrice }) => {
 
   useEffect(() => {
     if (orderBookData?.asks[0] && orderBookData?.bids[0]) {
+      console.log(sevenDigit((new BN(orderBookData.asks[0].price).plus(orderBookData.bids[0].price))
+        .div(2)
+        .toFixed(7)));
       setPrice(sevenDigit((new BN(orderBookData.asks[0].price).plus(orderBookData.bids[0].price))
         .div(2)
         .toFixed(7)));
+    } else {
+      setPrice(null);
     }
-  }, [orderBookData]);
+  }, [orderBookData, appSpotPair.base, appSpotPair.counter]);
 
   useEffect(() => {
     if (intervalRef.current) {
