3e3abcfb29f44dfc81e91fc731186df1997e0f54
Shayan Balaee <shayanfall79@gmail.com>
2021-10-06 15:27:25 +0330

    feat: add round info to detail page

--- src/containers/lottery/details/index.js
+++ src/containers/lottery/details/index.js
@@ -1,6 +1,12 @@
+import Image from 'next/image';
+import classNames from 'classnames';
 import Loading from 'components/Loading';
 import LotteryHead from 'containers/lottery/LotteryHead';
+import Button from 'components/Button';
 import { useEffect, useState } from 'react';
+import ArrowIcon from 'assets/images/arrow-right-icon.png';
+import RoundPrize from './RoundPrize';
+import RoundInfo from './RoundInfo';
 import LotteryHeader from '../LotteryHeader';
 import styles from '../style.module.scss';
 
@@ -13,11 +19,16 @@ const index = () => {
   useEffect(() => {
     async function fetchData() {
       const fetchedRound = await new Promise((resolve) => setTimeout(() => {
-        resolve([
+        resolve(
           { title: 'Round #1', status: 'Live', image: 'tesla.jpg' },
-        ]);
+        );
       }, 1500));
 
+      const fetchedTickets = await new Promise((resolve) => setTimeout(() => {
+        resolve([1, 2, 3, 4, 5]);
+      }, 1500));
+
+      setTickets(fetchedTickets);
       setRound(fetchedRound);
       setLoading(false);
     }
@@ -46,8 +57,26 @@ const index = () => {
       </div>
       <div className={styles.main}>
         <div className={styles.title}>
-          <h1 className={styles.board}>Board</h1>
+          <h1 className={classNames(styles.board, 'd-flex align-items-center')}>
+            Board
+            <span style={{ marginLeft: 12, marginTop: 3 }}>
+              <Image src={ArrowIcon} width={18} height={18} />
+            </span>
+            <span style={{ marginLeft: 12 }}>
+              {round.title}
+            </span>
+          </h1>
+          <Button htmlType="button" content="Buy Ticket" variant="primary" className={styles.button} />
         </div>
+        <div className="row mx-0 mt-3">
+          <div style={{ paddingLeft: 0 }} className="col-12 col-md-6">
+            <RoundPrize round={round} />
+          </div>
+          <div style={{ paddingLeft: 30 }} className="col-12 col-md-6">
+            <RoundInfo round={round} />
+          </div>
+        </div>
+        <div className="row mx-0 " />
       </div>
     </>
   );
