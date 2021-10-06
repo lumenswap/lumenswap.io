9c0fea32c45efbce4526b30faeff18669f28ea24
Shayan Balaee <shayanfall79@gmail.com>
2021-10-06 15:26:58 +0330

    refactor: move status to component

--- src/containers/lottery/board/RoundItem.js
+++ src/containers/lottery/board/RoundItem.js
@@ -1,26 +1,12 @@
 import Image from 'next/image';
-import classNames from 'classnames';
-
-import GiftIcon from 'assets/images/gift-icon.svg';
+import Status from '../Status';
 import styles from './style.module.scss';
 
 const RoundItem = ({ round }) => (
   <div className={styles.roundItem}>
     <div className="d-flex justify-content-between align-items-center">
       <span className={styles.title}>{round.title}</span>
-      <span className="d-flex align-items-center">
-        <Image src={GiftIcon} width={16} height={16} />
-        <div
-          style={{ marginLeft: 10 }}
-          className={classNames(
-            styles.status,
-            round.status === 'Live' ? styles.live : styles.ended,
-          )}
-        >
-          <span className={styles.dot} />
-          {round.status}
-        </div>
-      </span>
+      <Status round={round} gift />
     </div>
     <div className={styles.roundImage}>
       <Image src={`/images/${round.image}`} width={330} height={180} objectFit="contain" />
