6f639911375b76a21cdb8578c98e27a409efaadf
Shayan Balaee <shayanfall79@gmail.com>
2021-10-13 18:02:08 +0330

    lint: remove console.log

--- src/containers/lottery/board/index.js
+++ src/containers/lottery/board/index.js
@@ -13,7 +13,6 @@ const index = () => {
   useEffect(() => {
     async function fetchData() {
       const fetchedRounds = await getAllRounds();
-      console.log(fetchedRounds);
       setRounds(fetchedRounds.data);
       setLoading(false);
     }
