65a54d9af4b59c88a671694eb220f8cca2465e04
Ali Movahedi <ali_movahedi@aol.com>
2021-10-14 17:14:15 +0330

    add generate address url function

--- src/helpers/explorerURLGenerator.js
+++ src/helpers/explorerURLGenerator.js
@@ -3,3 +3,7 @@ const LUMENSCAN_URL = 'https://lumenscan.io';
 export function generateTransactionURL(tx) {
   return `${LUMENSCAN_URL}/txns/${tx}`;
 }
+
+export function generateAddressURL(account) {
+  return `${LUMENSCAN_URL}/account/${account}`;
+}
