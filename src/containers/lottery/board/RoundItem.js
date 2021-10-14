d923e95788fb8f8771d74927aa6c527aacf5bfa3
Ali Movahedi <ali_movahedi@aol.com>
2021-10-14 10:33:34 +0330

    fix loading prize image

--- src/containers/lottery/board/RoundItem.js
+++ src/containers/lottery/board/RoundItem.js
@@ -9,7 +9,7 @@ const RoundItem = ({ round }) => (
       <Status round={round} gift />
     </div>
     <div className={styles.roundImage}>
-      <Image src={`/images/${round?.prizeImage || 'tesla.jpg'}`} width={330} height={180} objectFit="contain" />
+      <Image src={`/${round?.prizeImage || 'tesla.jpg'}`} width={330} height={180} objectFit="contain" />
     </div>
   </div>
 );
