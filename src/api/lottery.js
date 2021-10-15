6c132035e6b9a8a2b961213c2290f82b5a0fcca6
Shayan Balaee <shayanfall79@gmail.com>
2021-10-15 20:16:12 +0330

    feat: new routes with query

--- src/api/lottery.js
+++ src/api/lottery.js
@@ -8,8 +8,8 @@ export function getAllRoundTickets(roundNumber) {
   return axios.get(`${process.env.REACT_APP_LUMEN_API}/lottery/round/${roundNumber}/tickets`);
 }
 
-export function getRoundParticipants(roundNumber) {
-  return axios.get(`${process.env.REACT_APP_LUMEN_API}/lottery/round/${roundNumber}/participants`);
+export function getRoundParticipants(roundNumber, query) {
+  return axios.get(`${process.env.REACT_APP_LUMEN_API}/lottery/round/${roundNumber}/participants`, { params: { ...query } });
 }
 
 export function getSingleRound(number) {
@@ -18,4 +18,15 @@ export function getSingleRound(number) {
 
 export function getMyTickets(address, roundNumber, page = 1) {
   return axios.get(`${process.env.REACT_APP_LUMEN_API}/lottery/ticket`, { params: { page, address, round: roundNumber } });
+}
+
+export function searchTikcets(query, roundNumber) {
+  return axios
+    .get(`${process.env.REACT_APP_LUMEN_API}/lottery/ticket`,
+      {
+        params: {
+          round: roundNumber,
+          ...query,
+        },
+      });
 }
