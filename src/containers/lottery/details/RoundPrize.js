180294f7dc5bb0919fa4fc88f9e1a13019a98d3e
shayan balaee <shayanfall79@gmail.com>
2021-10-16 17:09:26 +0330

    add new hover overlay for prize image

--- src/containers/lottery/details/RoundPrize.js
+++ src/containers/lottery/details/RoundPrize.js
@@ -4,6 +4,11 @@ import styles from './style.module.scss';
 
 const RoundPrize = ({ round }) => (
   <div className={styles.roundItem}>
+    <div className={styles['description-overlay']}>
+      <p>
+        {round?.prizeDescription}
+      </p>
+    </div>
     <div className="d-flex justify-content-end align-items-center">
       <Status round={round} />
     </div>
