d338248c14d988267d3d5e2c2bde2caa84a836ab
Ali Movahedi <ali_movahedi@aol.com>
2021-10-25 05:59:05 +0330

    add nice token

---
+++ src/tokens/NICE.js
@@ -0,0 +1,8 @@
+import logo from './images/nice.png';
+
+export default {
+  code: 'NICE',
+  logo,
+  web: 'nicetrade.co',
+  issuer: 'GCLQNUO2P5AFUEOYZZLOMBD2BUDLFFIV73VRRMAJ3CGPPUL7T7SONICE',
+};
