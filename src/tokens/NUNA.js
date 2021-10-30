62f7257da805e4b3e570f474984fa0eb2cc3b663
Ali Movahedi <ali_movahedi@aol.com>
2021-10-30 15:43:08 +0330

    add nuna token

---
+++ src/tokens/NUNA.js
@@ -0,0 +1,8 @@
+import logo from './images/nuna.png';
+
+export default {
+  code: 'NUNA',
+  logo,
+  web: 'nunaproject.art',
+  issuer: 'GCX2ENOVSSOOH6G4HIOBMPCBFXHDVDGA546NK3ZFX3NP3QS25BKZBWOW',
+};
