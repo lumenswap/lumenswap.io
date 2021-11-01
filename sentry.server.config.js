f92136c6447674c0493b7d8ce8eb2a6dc432f959
shayanbalaee <shayanfall79@gmail.com>
2021-11-01 23:18:16 +0330

    change sentry config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://08c9ef8f8a9e41628164018ef2c629e0@o1057225.ingest.sentry.io/6043912',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
