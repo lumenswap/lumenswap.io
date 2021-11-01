9e71111357f77256e8b26eb8445bd5b47a681c7d
Shayan Balaee <shayanfall79@gmail.com>
2021-11-01 16:12:07 +0330

    setup sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 const withTM = require('next-transpile-modules')(['redux-persist']);
 const withImages = require('next-images');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withImages(withTM({
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(withImages(withTM({
   env: {
     REACT_APP_HORIZON: process.env.REACT_APP_HORIZON,
     REACT_APP_METRIC_SERVER: process.env.REACT_APP_METRIC_SERVER,
@@ -17,4 +30,4 @@ module.exports = withImages(withTM({
   images: {
     domains: ['cdn.lumenswap.io'],
   },
-}));
+})), sentryWebpackPluginOptions);
