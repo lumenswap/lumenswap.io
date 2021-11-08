32fe8770960a706e4e805b2c87f78651ea9edd21
mersedee <mersede.rahbar@gmail.com>
2021-11-08 12:01:29 +0330

    add header

--- src/containers/auction/detail/index.js
+++ src/containers/auction/detail/index.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import Head from 'next/head';
+import classNames from 'classnames';
 
-const AuctionDetail = () => (
-  <div>
-    detail
-  </div>
-);
+import urlMaker from 'helpers/urlMaker';
+import AuctionHeader from 'components/AuctionHeader';
+import Breadcrumb from 'components/BreadCrumb';
+import Button from 'components/Button';
+
+import styles from './styles.module.scss';
+
+const AuctionDetail = () => {
+  const Container = ({ children }) => (
+    <div className="container-fluid">
+      <Head>
+        <title>Auction Board | Lumenswap</title>
+      </Head>
+      <AuctionHeader />
+      {children}
+    </div>
+  );
+
+  const breadCrumbData = [
+    {
+      name: 'Auction',
+      url: urlMaker.auction.root(),
+    },
+    {
+      render: () => (<div>Rabet(RBT)</div>),
+    },
+  ];
+
+  return (
+    <Container>
+      <div className={classNames('layout main', styles.layout)}>
+        <div className="row justify-content-center">
+          <div className="col-xl-8 col-lg-10 col-md-11 col-sm-12 col-12">
+            <div className="d-flex justify-content-between align-items-center">
+              <Breadcrumb
+                data={breadCrumbData}
+              />
+              <Button
+                className={styles.btn}
+                content="Send Bid"
+                variant="primary"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default AuctionDetail;
