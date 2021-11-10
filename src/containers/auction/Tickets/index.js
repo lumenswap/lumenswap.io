34dbabe9ca83f947fa4f2098faf0e3cf10080a8d
mersedee <mersede.rahbar@gmail.com>
2021-11-10 10:13:51 +0330

    add select option

--- src/containers/auction/Tickets/index.js
+++ src/containers/auction/Tickets/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import Head from 'next/head';
+import { useState } from 'react';
+import classNames from 'classnames';
 
-const AuctionTickets = () => (
-  <div>
-    hiii
-  </div>
-);
+import AuctionHeader from 'components/AuctionHeader';
+import SelectOption from 'components/SelectOption';
+
+import styles from './styles.module.scss';
+
+const AuctionTickets = () => {
+  const dropdownItems = [
+    { value: 'rbt', label: 'Rabet (RBT)' },
+    { value: 'lsp', label: 'Lumenswap (LSP)' },
+  ];
+  const [selectedItem, setSelectedItem] = useState(dropdownItems[0]);
+
+  const Container = ({ children }) => (
+    <div className="container-fluid">
+      <Head>
+        <title>Auction Tickets | Lumenswap</title>
+      </Head>
+      <AuctionHeader />
+      {children}
+    </div>
+  );
+
+  return (
+    <Container>
+      <div className={classNames('layout main', styles.layout)}>
+        <div className="row justify-content-center">
+          <div className="col-xl-8 col-lg-10 col-md-11 col-sm-12 col-12">
+            <div className="d-flex justify-content-between align-items-center">
+              <h1 className={styles.title}>My Bids</h1>
+              <SelectOption
+                items={dropdownItems}
+                setValue={setSelectedItem}
+                className={styles.filter}
+                isSearchable={false}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default AuctionTickets;
