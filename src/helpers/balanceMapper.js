e000bf78b1daf23b1cd667070e9168c91fda2337
Ali Movahedi <ali_movahedi@aol.com>
2021-11-18 23:55:30 +0330

    change available user balance

--- src/helpers/balanceMapper.js
+++ src/helpers/balanceMapper.js
@@ -1,11 +1,17 @@
+import BN from 'helpers/BN';
 import getAssetDetails from './getAssetDetails';
 
 export default function balanceMapper(item) {
+  const balance = new BN(item.balance)
+    .minus(item.selling_liabilities)
+    .toFixed(7);
+
   return {
     asset: getAssetDetails({
       code: item.asset_code,
       issuer: item.asset_issuer,
     }),
-    balance: item.balance,
+    balance,
+    rawBalance: item.balance,
   };
 }
