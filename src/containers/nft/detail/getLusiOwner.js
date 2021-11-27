af270eb7d6f03c150ef2e77f5a09a544cfb430a2
Shayan Balaee <shayanfall79@gmail.com>
2021-11-27 20:23:58 +0330

    check for balance gt 0

--- src/containers/nft/detail/getLusiOwner.js
+++ src/containers/nft/detail/getLusiOwner.js
@@ -1,14 +1,15 @@
 import { getAccounts } from 'api/nft';
+import BN from 'helpers/BN';
 
 export default function getLusiOwner(lusiId) {
   return getAccounts(lusiId).then(async (res) => {
     if (res.data._embedded.records.length > 0) {
       for (const account of res.data._embedded.records) {
         for (const balance of account.balances) {
-          if
-          (
+          if (
             balance.asset_code === lusiId
-                    && balance.asset_issuer === process.env.REACT_APP_LUSI_ISSUER
+            && balance.asset_issuer === process.env.REACT_APP_LUSI_ISSUER
+            && new BN(balance.balance).gt(0)
           ) {
             return {
               address: account.account_id,
@@ -20,16 +21,17 @@ export default function getLusiOwner(lusiId) {
       }
 
       const nextPageAccounts = await getAccounts(
-        lusiId, res.data._embedded.records[0].paging_token,
+        lusiId,
+        res.data._embedded.records[0].paging_token,
       );
 
       if (nextPageAccounts.data._embedded.records.length > 0) {
         for (const account of nextPageAccounts.data._embedded.records) {
           for (const balance of account.balances) {
-            if
-            (
+            if (
               balance.asset_code === lusiId
-                        && balance.asset_issuer === process.env.REACT_APP_LUSI_ISSUER
+              && balance.asset_issuer === process.env.REACT_APP_LUSI_ISSUER
+              && new BN(balance.balance).gt(0)
             ) {
               return {
                 address: account.account_id,
