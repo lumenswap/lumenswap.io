9ea9c7614dae81fb3c878e98977afbf08db4ffe1
Amirrk79 <amirmntmntmntmnt@gmail.com>
2021-12-11 17:21:44 +0330

    fix confirm swap modal bug

--- src/blocks/ConfirmSwap/index.js
+++ src/blocks/ConfirmSwap/index.js
@@ -8,7 +8,7 @@ import fetchMarketPrice from 'helpers/fetchMarketPrice';
 import isSameAsset from 'helpers/isSameAsset';
 import generateSwapTRX from 'stellar-trx/generateSwapTRX';
 import { loginTypes } from 'reducers/user';
-import ColorizedPriceImpact from 'components/complex/LumenSwapSwap';
+import ColorizedPriceImpact from 'components/complex/LumenSwapSwap/ColorizedPriceImpact';
 import appConsts from 'appConsts';
 import showSignResponse from 'helpers/showSignResponse';
 import showGenerateTrx from 'helpers/showGenerateTrx';
