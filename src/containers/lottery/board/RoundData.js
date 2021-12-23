c3d5177dbc738d492bcf71bf7490d3e15ae2db2e
Amirrk79 <amirmntmntmntmnt@gmail.com>
2021-12-23 20:08:59 +0330

    change lottery styles

--- src/containers/lottery/board/RoundData.js
+++ src/containers/lottery/board/RoundData.js
@@ -1,16 +1,19 @@
+import classNames from 'classnames';
 import urlMaker from 'helpers/urlMaker';
 import Link from 'next/link';
 import RoundItem from './RoundItem';
 import styles from './style.module.scss';
 
 const RoundData = ({ rounds }) => (
-  <div className={styles.roundsList}>
+  <div className={classNames(styles.roundsList, 'row')}>
     {rounds.map((round, i) => (
-      <Link key={i} href={urlMaker.lottery.round.root(round.number)} passHref>
-        <a className="text-decoration-none">
-          <RoundItem round={round} />
-        </a>
-      </Link>
+      <div className="col-12 col-lg-6">
+        <Link key={i} href={urlMaker.lottery.round.root(round.number)} passHref>
+          <a className="text-decoration-none">
+            <RoundItem round={round} />
+          </a>
+        </Link>
+      </div>
     ))}
   </div>
 );
--- src/containers/lottery/board/RoundItem.js
+++ src/containers/lottery/board/RoundItem.js
@@ -3,32 +3,50 @@ import numeral from 'numeral';
 import Status from '../Status';
 import styles from './style.module.scss';
 
-const RoundItem = ({ round }) => (
-  <div className={styles['round-item-main']}>
-    <div className={styles.roundItem}>
-      <div className="d-flex justify-content-between align-items-center">
-        <span className={styles.title}>Round #{round.number}</span>
-        <Status round={round} />
-      </div>
-      <div className={styles.roundImage}>
-        {round?.prizeImage && <img src={round?.prizeImage} style={{ width: 330, height: 180, objectFit: 'contain' }} />}
-      </div>
-    </div>
-    <div className={styles['round-item-info']}>
-      <div className={styles['round-item-info-section']}>
-        <div className={styles['round-item-info-texts']}>
-          <span className={styles['round-item-info-texts-number']}>{numeral(round.participantCount).format('0,0')}</span>
-          <span className={styles['round-item-info-texts-text']}>Participants</span>
+const RoundItem = ({ round }) => {
+  let isNotStarted = false;
+  if (round.status.toLowerCase() === 'not started') {
+    isNotStarted = true;
+  }
+
+  let imageSizes = [330, 180];
+  if (isNotStarted) {
+    imageSizes = [270, 150];
+  }
+  return (
+    <div className={styles['round-item-main']}>
+      <div className={styles.roundItem}>
+        <div className="d-flex justify-content-between align-items-center">
+          <span className={styles.title}>Round #{round.number}</span>
+          <Status round={round} />
+        </div>
+        <div className={styles.roundImage}>
+          {round?.prizeImage && (
+          <img
+            src={round?.prizeImage}
+            style={{
+              width: imageSizes[0], height: imageSizes[1], objectFit: 'contain', marginTop: `${isNotStarted ? '15px' : '0px'}`,
+            }}
+          />
+          )}
         </div>
       </div>
-      <div className={styles['round-item-info-section']}>
-        <div className={styles['round-item-info-texts']}>
-          <span className={styles['round-item-info-texts-number']}>{numeral(round.ticketCount).format('0,0')}</span>
-          <span className={styles['round-item-info-texts-text']}>Tickets</span>
+      <div className={styles['round-item-info']}>
+        <div className={styles['round-item-info-section']}>
+          <div className={styles['round-item-info-texts']}>
+            <span className={styles['round-item-info-texts-number']}>{isNotStarted ? '- -' : numeral(round.participantCount).format('0,0')}</span>
+            <span className={styles['round-item-info-texts-text']}>Participants</span>
+          </div>
+        </div>
+        <div className={styles['round-item-info-section']}>
+          <div className={styles['round-item-info-texts']}>
+            <span className={styles['round-item-info-texts-number']}>{isNotStarted ? '- -' : numeral(round.ticketCount).format('0,0')}</span>
+            <span className={styles['round-item-info-texts-text']}>Tickets</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RoundItem;
