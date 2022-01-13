5cdbec1f1ecb1c83290176f07e4e25e7d802d285
mersedee <mersede.rahbar@gmail.com>
2022-01-13 17:17:59 +0330

    add description section

--- src/containers/dao/proposalInfo/index.js
+++ src/containers/dao/proposalInfo/index.js
@@ -60,6 +60,32 @@ const ProposalInfo = () => {
                   Vote <ArrowIcon />
                 </Button>
               </div>
+
+              <div className={classNames(styles.card, 'mt-4')}>
+                <h4 className={styles['card-title-small']}>Description</h4>
+                <p className={styles['card-desc']}>
+                  This proposal brings to a formal vote the Retroactive Distribution,
+                  discussed at length in the above links.
+                  <div className="d-block my-3" />
+                  This proposal retroactively distributes 400 UNI to 12,619 distinct
+                  addresses who interacted with Uniswap via a proxy contract. These
+                  12,619 users are "Phase 1" of the Retroactive Distribution,
+                  encompassing users of application-integrations. All of these
+                  12,619 addresses were excluded from the original airdrop.
+                  <div className="d-block my-3" />
+                  The Phase determination was made based on how easy it is to
+                  programmatically hook a trading bot into them, as this is a
+                  proxy for what portion of these cohorts risk representing multiple
+                  addresses per end-user. Phase 1 is the less programmatically
+                  accessible cohort, indicating a lower likelihood of multiple
+                  addresses per end-user.
+                  <div className="d-block my-3" />
+                  Specifically, this proposal transfers 5,047,600 UNI to a new
+                  MerkleDistributor contract, which will then allow for 400 UNI
+                  to be claimed by each of the 12,619 accounts held by users of
+                  the following projects:
+                </p>
+              </div>
             </div>
           </div>
         </div>
