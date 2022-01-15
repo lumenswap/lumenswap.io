bb5b9a1cd8c4e14a6e5114352f75097dbebc0379
mersedee <mersede.rahbar@gmail.com>
2022-01-15 15:09:03 +0330

    add table

--- src/containers/dao/votes/index.js
+++ src/containers/dao/votes/index.js
@@ -6,6 +6,8 @@ import ServerSideLoading from 'components/ServerSideLoading';
 import DAOHeader from 'containers/dao/DAOHeader';
 import Breadcrumb from 'components/BreadCrumb';
 import urlMaker from 'helpers/urlMaker';
+import CTable from 'components/CTable';
+import NoData from 'components/NoData';
 
 import styles from './styles.module.scss';
 
@@ -28,6 +30,37 @@ const Votes = () => {
     { url: `${urlMaker.dao.singleDao.proposalInfo(router.query.name)}`, name: 'Proposal info' },
     { name: 'All votes' },
   ];
+
+  const tableInfo = [
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: '1',
+      render: () => (
+        <a
+          className="text-decoration-none"
+          target="_blank"
+          rel="noreferrer"
+        >3P2p…rb4P
+        </a>
+      ),
+    },
+    {
+      title: 'Vote',
+      dataIndex: 'vote',
+      key: '2',
+      render: () => (<>Yes</>),
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: '3',
+      render: () => (<>100 RBT</>),
+    },
+  ];
+
+  const NoDataMessage = () => (<NoData message="There is no votes" />);
+
   return (
     <Container>
       <ServerSideLoading>
@@ -38,6 +71,18 @@ const Votes = () => {
                 spaceBetween={8}
                 data={crumbData}
               />
+
+              <div className={styles.card}>
+                <h4 className={styles.title}>Votes
+                </h4>
+                <CTable
+                  className={styles.table}
+                  columns={tableInfo}
+                  dataSource={Array(20).fill({})}
+                  noDataComponent={NoDataMessage}
+                  rowFix={{ rowNumbers: 20, rowHeight: 53, headerRowHeight: 25 }}
+                />
+              </div>
             </div>
           </div>
         </div>
