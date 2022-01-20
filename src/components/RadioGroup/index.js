48bed704f76bc4730853e9fbcf9d96004f8f7d95
mersedee <mersede.rahbar@gmail.com>
2022-01-20 15:00:22 +0330

    add radio group component

---
+++ src/components/RadioGroup/index.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+
+const RadioGroup = ({
+  value, onUpdate, items, name,
+}) => {
+  const onChange = (e) => {
+    onUpdate(e.target.value);
+  };
+
+  return (
+    <div className={styles['radio-group']}>
+      {items.map((item) => (
+        <label key={item.value}>
+          <input
+            type="radio"
+            checked={value === item.value}
+            disabled={item.disabled}
+            value={item.value}
+            name={name}
+            onChange={onChange}
+          />
+          <span>{item.label}</span>
+        </label>
+      ))}
+    </div>
+  );
+};
+
+RadioGroup.propTypes = {
+
+};
+
+export default RadioGroup;
--- src/containers/dao/proposalInfo/Vote/index.js
+++ src/containers/dao/proposalInfo/Vote/index.js
@@ -1,17 +1,29 @@
 import PropTypes from 'prop-types';
 import { useForm, Controller } from 'react-hook-form';
+import { useState } from 'react';
 
 import Button from 'components/Button';
 import InputGroup from 'components/InputGroup';
+import RadioGroup from 'components/RadioGroup';
 
 import styles from './styles.module.scss';
 
 const Index = () => {
+  const items = [
+    { value: 'yes', label: 'Yes' },
+    { value: 'no', label: 'No' },
+  ];
+  const [radioValue, setRadioValue] = useState(items[0].value);
   const {
     handleSubmit, control,
   } = useForm({ mode: 'onChange' });
 
   async function onSubmit(data) { console.warn(data); }
+
+  function onChange(value) {
+    setRadioValue(value);
+    console.log(value);
+  }
 
   return (
     <div className="pb-4 main">
@@ -19,6 +31,15 @@ const Index = () => {
         Will Joe Biden win the 2020 United States presidential election?
       </p>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className="my-4">
+          <RadioGroup
+            items={items}
+            name="opt-group"
+            value={radioValue}
+            className="radio-group"
+            onUpdate={onChange}
+          />
+        </div>
         <hr className={styles.hr} />
         <label className="label-primary mb-1">Amount</label>
         <Controller
