ebf596b5adb6e634692dee1d9fb64d9340a02c27
Shayan Balaee <shayanfall79@gmail.com>
2022-02-17 11:24:07 +0330

    fetch community member count in for single governance

--- src/containers/dao/DAOProposalList/props.js
+++ src/containers/dao/DAOProposalList/props.js
@@ -1,10 +1,12 @@
 import { getGovernanceProposals, getGovernances } from 'api/daoAPI';
+import { listAssets } from 'api/stellar';
+import { getAssetDetails } from 'helpers/asset';
 
 export async function daoProposalsGetServerSideProps({ params }) {
   try {
     const governanceInfoResponse = await getGovernances({ name: params.name });
 
-    if (!governanceInfoResponse) {
+    if (!governanceInfoResponse || !governanceInfoResponse.length) {
       return {
         notFound: true,
       };
@@ -12,12 +14,29 @@ export async function daoProposalsGetServerSideProps({ params }) {
 
     const governanceInfo = governanceInfoResponse[0];
 
+    const assetDetails = getAssetDetails({
+      code: governanceInfo.assetCode,
+      issuer: governanceInfo.assetIssuer,
+    });
+
+    const assets = await listAssets({ asset_code: assetDetails.code });
+
+    const assetData = assets._embedded.records.find(
+      (asset) => asset.asset_code
+      === governanceInfo.assetCode && asset.asset_issuer === governanceInfo.assetIssuer,
+    );
+
+    const communityMembersCount = assetData?.num_accounts || 0;
+
     const proposals = await getGovernanceProposals(governanceInfo.id);
 
     return {
       props: {
         proposals,
-        governanceInfo,
+        governanceInfo: {
+          ...governanceInfo,
+          communityMembersCount,
+        },
       },
     };
   } catch (e) {
