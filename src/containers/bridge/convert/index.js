773342c7dffbf5ac6f942ea85a4840de202b3e70
mersedee <mersede.rahbar@gmail.com>
2022-02-22 15:22:48 +0330

    add convert page

--- src/containers/bridge/convert/index.js
+++ src/containers/bridge/convert/index.js
@@ -1,15 +1,97 @@
-import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import BridgeContainer from 'containers/bridge/BridgeContainer';
+import Button from 'components/Button';
+import btcSrc from 'assets/images/btc-logo.png';
+import Input from 'components/Input';
 import styles from './styles.module.scss';
 
-const BridgeConvert = () => (
-  <div>
+const BridgeConvert = () => {
+  const {
+    handleSubmit,
+    control,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const onSubmit = (data) => { console.warn(data); };
+
+  return (
     <BridgeContainer title="Bridge Convert | Lumenswap">
-      <div className="layout main">
-        conevrt
+      <div className="layout main d-flex justify-content-center">
+        <div className={styles.card}>
+          <div className={styles.container}>
+            <Button
+              variant="basic"
+              size="100%"
+              className={styles['convert-btn']}
+            >
+              <div className="d-flex align-items-center">
+                <img src={btcSrc} width={30} height={30} alt="logo" />
+                <div className={styles.currency}>BTC</div>
+              </div>
+              <div className="icon-angle-down color-base" />
+            </Button>
+
+            <div className={styles.icon}>
+              <span className="icon-arrow-down color-primary" />
+            </div>
+
+            <Button
+              variant="basic"
+              size="100%"
+              className={styles['convert-btn']}
+            >
+              <div className="d-flex align-items-center">
+                <img src={btcSrc} width={30} height={30} alt="logo" />
+                <div className={styles.currency}>ETH</div>
+              </div>
+              <div className="icon-angle-down color-base" />
+            </Button>
+          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="label-primary mt-3">Amount</label>
+            <Controller
+              name="amount"
+              control={control}
+              render={(props) => (
+                <Input
+                  type="number"
+                  placeholder="1"
+                  value={props.value}
+                  onChange={props.onchange}
+                />
+              )}
+            />
+
+            <label className="label-primary mt-3">Destination</label>
+            <Controller
+              name="destination"
+              control={control}
+              render={(props) => (
+                <Input
+                  type="text"
+                  placeholder="G …"
+                  value={props.value}
+                  onChange={props.onchange}
+                />
+              )}
+            />
+
+            <Button
+              variant="primary"
+              htmlType="submit"
+              size="100%"
+              fontWeight={500}
+              className="mt-4"
+            >
+              Convert
+            </Button>
+          </form>
+        </div>
       </div>
     </BridgeContainer>
-  </div>
-);
+  );
+};
 
 export default BridgeConvert;
