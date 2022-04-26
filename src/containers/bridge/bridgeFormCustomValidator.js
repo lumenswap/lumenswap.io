191fd5cf7cfb89a807ea0e7a67c8ef2c8abe1c71
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-04-26 22:57:48 +0430

    add some validation for bridge amount

--- src/containers/bridge/bridgeFormCustomValidator.js
+++ src/containers/bridge/bridgeFormCustomValidator.js
@@ -1,13 +1,25 @@
 import walletValidator from 'multicoin-address-validator';
 import generateFormResolverErrors from 'helpers/generateFormResolverErrors';
 import BN from 'helpers/BN';
+import { getAssetDetails, isSameAsset } from 'helpers/asset';
 import { TOKEN_A_FORM_NAME, TOKEN_B_FORM_NAME } from './tokenFormNames';
 
-function bridgeFormCustomValidator(formValues) {
+function bridgeFormCustomValidator(formValues, userBalances) {
   const currentToToken = formValues[TOKEN_B_FORM_NAME];
   const currentFromToken = formValues[TOKEN_A_FORM_NAME];
+  if (currentFromToken.network === 'stellar' && userBalances) {
+    const foundAssetInUserBalances = userBalances?.find((balance) => isSameAsset(balance.asset,
+      getAssetDetails({ code: currentFromToken.name, issuer: process.env.REACT_APP_L_ISSUER })));
+    const fromAssetUserAmount = foundAssetInUserBalances?.balance ?? 0;
+    if (new BN(formValues.amount).gt(fromAssetUserAmount)) {
+      return { values: formValues, errors: generateFormResolverErrors('amount', 'Insufficient amount') };
+    }
+  }
   if (!formValues.amount || formValues.amount === '') {
     return { values: formValues, errors: generateFormResolverErrors('amount', 'Amount is required') };
+  }
+  if (new BN(formValues.amount).isEqualTo(0)) {
+    return { values: formValues, errors: generateFormResolverErrors('amount', 'Invalid amount') };
   }
   if (!new BN(formValues.amount).gte(currentFromToken.minimum_amount)) {
     return { values: formValues, errors: generateFormResolverErrors('amount', `Minimum amount is ${currentFromToken.minimum_amount}`) };
