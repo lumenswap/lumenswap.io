ea1ff6486b7dfad26e4bdd6e7d93f2f39375748c
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-04-27 14:48:52 +0430

    add l issuer env to next config file

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ module.exports = withImages(withTM({
     REACT_APP_LOTTERY_ACCOUNT: process.env.REACT_APP_LOTTERY_ACCOUNT,
     REACT_APP_LUSI_ISSUER: process.env.REACT_APP_LUSI_ISSUER,
     REACT_APP_DAO_LOCKER_ADDRESS: process.env.REACT_APP_DAO_LOCKER_ADDRESS,
+    REACT_APP_L_ISSUER: process.env.REACT_APP_L_ISSUER,
   },
   webpack5: true,
   images: {
