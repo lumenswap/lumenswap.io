bc4f4e5af1c96dcadc6648603db0fbc9d20d8b29
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-05-05 14:00:09 +0430

    remove console.log

--- src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/ConfirmTransactionLoading/index.js
+++ src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/ConfirmTransactionLoading/index.js
@@ -4,29 +4,26 @@ import ExternalIcon from 'assets/images/external';
 import capitalizeFirstLetter from 'helpers/capitalizeFirstLetter';
 import styles from '../../styles.module.scss';
 
-const ConfirmTransactionLoading = ({ convertInfo, transactionInfo }) => {
-  console.log(convertInfo, transactionInfo);
-  return (
-    <>
-      <p className={styles.text}>
-        Your transaction has been done.
-        {' '} Please wait for confirmation by {capitalizeFirstLetter(convertInfo.to_asset.network)}.
-      </p>
-      <div className={classNames(styles.note, styles['note-link'])}>
-        Tx hash
-        <br />
-        <a
-          href="/"
-          target="_blank"
-          rel="noreferrer"
-          className="color-primary"
-        >
-          {transactionInfo.tx_hash}
-          <ExternalIcon />
-        </a>
-      </div>
-    </>
-  );
-};
+const ConfirmTransactionLoading = ({ convertInfo, transactionInfo }) => (
+  <>
+    <p className={styles.text}>
+      Your transaction has been done.
+      {' '} Please wait for confirmation by {capitalizeFirstLetter(convertInfo.to_asset.network)}.
+    </p>
+    <div className={classNames(styles.note, styles['note-link'])}>
+      Tx hash
+      <br />
+      <a
+        href="/"
+        target="_blank"
+        rel="noreferrer"
+        className="color-primary"
+      >
+        {transactionInfo.tx_hash}
+        <ExternalIcon />
+      </a>
+    </div>
+  </>
+);
 
 export default ConfirmTransactionLoading;
