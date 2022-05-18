3f36d630010fa2b92001761d26dab5d74b5423b5
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-05-18 17:07:08 +0430

    customize dark mode btn

--- src/components/ToggleDarkModeBtn/index.js
+++ src/components/ToggleDarkModeBtn/index.js
@@ -1,9 +1,22 @@
 import SunIcon from 'assets/images/sun';
-import { useEffect, useState } from 'react';
+import SunLightIcon from 'assets/images/sunLight';
+import MoonIcon from 'assets/images/moon';
+import MoonLightIcon from 'assets/images/moonLight';
+import { useEffect, useState, useRef } from 'react';
 import styles from './styles.module.scss';
 
+function setDocumentElementAttribute(attribute, timeOut) {
+  setTimeout(() => {
+    if (attribute === 'light') {
+      document.documentElement.removeAttribute('data-theme');
+    } else if (attribute === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+  }, timeOut ?? 0);
+}
 function ToggleDarkModeBtn() {
   const [theme, setTheme] = useState('light');
+  const firstTimeRef = useRef(true);
 
   useEffect(() => {
     const currentTheme = localStorage.getItem('theme');
@@ -12,13 +25,10 @@ function ToggleDarkModeBtn() {
     }
   }, []);
   useEffect(() => {
-    if (theme === 'light') {
-      document.documentElement.removeAttribute('data-theme');
-    } else if (theme === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    }
+    setDocumentElementAttribute(theme, firstTimeRef.current ? 0 : 400);
   }, [theme]);
   const handleChangeTheme = () => {
+    firstTimeRef.current = false;
     if (theme === 'light') {
       setTheme('dark');
       localStorage.setItem('theme', 'dark');
@@ -32,8 +42,8 @@ function ToggleDarkModeBtn() {
       <div className={styles['main-relative']}>
         <div className={theme === 'light' ? styles.toggle : styles.toggled} />
         <div className={styles['icon-container']}>
-          <div className={styles.icon}><SunIcon /></div>
-          <div className={styles.icon}><SunIcon /></div>
+          <div className={styles.icon}>{theme === 'light' ? <SunLightIcon /> : <SunIcon />}</div>
+          <div className={styles.icon}>{theme === 'light' ? <MoonLightIcon /> : <MoonIcon />}</div>
         </div>
       </div>
     </div>
