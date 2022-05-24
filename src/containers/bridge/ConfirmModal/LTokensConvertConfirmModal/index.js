88298ff2765ba5d577059aa4ff01640ebe592a22
Shayan Balaee <shayanfall79@gmail.com>
2022-05-24 21:31:13 +0430

    remove extra tx signing loading

--- src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/index.js
+++ src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/index.js
@@ -3,15 +3,14 @@ import CSteps from 'components/CSteps';
 import SuccessDialog from 'containers/bridge/ConfirmModal/SuccessDialog';
 import generatePaymentTRX from 'stellar-trx/generatePaymentTRX';
 import { useDispatch } from 'react-redux';
-import ConvertConfirmModalContent from 'containers/bridge/ConvertConfirmModalContent';
 import useUserAddress from 'hooks/useUserAddress';
 import { getAssetDetails } from 'helpers/asset';
-import showGenerateTrx from 'helpers/showGenerateTrx';
-import showSignResponse from 'helpers/showSignResponse';
 import { openModalAction } from 'actions/modal';
 import { orderStates } from 'containers/bridge/orderStates';
 import getSingleOrder from 'api/birdgeAPI/getSingleOrder';
 import { calculateFromAmount } from 'containers/bridge/calculateFromAndToAmounts';
+import TransactionResponse from 'blocks/TransactionResponse';
+import signForThem from 'walletIntegeration/signForThem';
 import ConfirmLTokenTransaction from './ConfirmLTokenTransaction';
 import ConfirmTransactionLoading from './ConfirmTransactionLoading';
 import styles from '../styles.module.scss';
@@ -53,22 +52,10 @@ const LTokensConvertCofirmModal = ({ convertInfo, defaultSignLoading = false }) 
     }
   }, [currentConvertInfo]);
 
-  const handleCloseSignModal = () => {
-    dispatch(openModalAction({
-      modalProps: {
-        className: 'main p-0',
-        hasClose: false,
-      },
-      content: <ConvertConfirmModalContent
-        defaultSignLoading
-        convertInfo={currentConvertInfo}
-      />,
-    }));
-  };
-
-  const sendConvertRequest = () => {
-    async function generateTRXHandler() {
-      return generatePaymentTRX(
+  const sendConvertRequest = async () => {
+    try {
+      setSignLoading(true);
+      const trx = await generatePaymentTRX(
         userAddress,
         calculateFromAmount(currentConvertInfo),
         getAssetDetails({
@@ -77,11 +64,20 @@ const LTokensConvertCofirmModal = ({ convertInfo, defaultSignLoading = false }) 
         currentConvertInfo.wallet.address,
         currentConvertInfo.memo,
       );
-    }
 
-    showGenerateTrx(generateTRXHandler, dispatch)
-      .then(async (trx) => showSignResponse(trx, dispatch, handleCloseSignModal))
-      .catch(console.error);
+      await signForThem(trx, () => {});
+    } catch (e) {
+      console.error(e);
+      setSignLoading(false);
+      dispatch(openModalAction({
+        modalProps: {},
+        content: <TransactionResponse
+          message={e.message}
+          status="failed"
+          title="Failed"
+        />,
+      }));
+    }
   };
 
   const convertSteps = [
