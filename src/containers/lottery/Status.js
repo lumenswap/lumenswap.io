b6087091ff2196354181303f5f5760c5ba3ef7e1
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-05-31 23:33:33 +0430

    fix lottery dark mode

--- src/containers/lottery/Status.js
+++ src/containers/lottery/Status.js
@@ -1,8 +1,11 @@
 import classNames from 'classnames';
 import GiftIcon from 'assets/images/gift-icon.svg';
+import GiftDarkIcon from 'assets/images/gift-dark.svg';
+import useCurrentTheme from 'hooks/useCurrentTheme';
 import styles from './style.module.scss';
 
 const Status = ({ round }) => {
+  const currentTheme = useCurrentTheme();
   let statusClassName = 'live';
   if (round.status.toLowerCase() === 'ended') {
     statusClassName = 'ended';
@@ -12,7 +15,7 @@ const Status = ({ round }) => {
 
   return (
     <span className="d-flex align-items-center">
-      {round.status.toLowerCase() === 'live' && <img src={GiftIcon} width={20} height={20} />}
+      {round.status.toLowerCase() === 'live' && <img src={currentTheme === 'light' ? GiftIcon : GiftDarkIcon} width={20} height={20} />}
       <div
         style={{ marginLeft: 14 }}
         className={classNames(
