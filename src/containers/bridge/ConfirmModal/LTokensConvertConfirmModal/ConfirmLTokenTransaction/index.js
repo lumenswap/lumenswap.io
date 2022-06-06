a8546610227fb9ec5ea8968c5b5dba29e8ed542e
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-06-06 21:14:08 +0430

    change sign button text bridge

--- src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/ConfirmLTokenTransaction/index.js
+++ src/containers/bridge/ConfirmModal/LTokensConvertConfirmModal/ConfirmLTokenTransaction/index.js
@@ -37,7 +37,7 @@ const ConfirmLTokenTransaction = ({ sendConvertRequest, convertInfo, signLoading
 
       <Button
         variant="primary"
-        content="Sign"
+        content="Sign & Send"
         size="100%"
         className="mt-5"
         onClick={handleSign()}
