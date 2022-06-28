bd18589a4ef019b416bc14deba2bdc9de00a6c65
Shayan Balaee <shayanfall79@gmail.com>
2022-06-28 13:22:51 +0430

    fetch assets from server url

--- src/api/assets/index.js
+++ src/api/assets/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getDefaultAssets() {
-  const assets = await axios.get(`${process.env.REACT_APP_LUMEN_API}/asset`);
+  const assets = await axios.get(`${process.env.TOKENS_SERVER_URL}/asset`);
   return assets.data;
 }
 
